0f9c0239c07f662dcf244db8b86459765eb46fd6
“ShellyXueHan” <shelly@freshworks.io>
2018-03-16 15:42:21 -0700

    other views

---
+++ geb-mobile/Myra-iOS-BDD-test/src/test/groovy/geb/mobile/ios/views/LoginView.groovy
@@ -0,0 +1,15 @@
+package geb.mobile.ios.views
+
+import geb.mobile.ios.IosBaseView
+
+class LoginView extends IosBaseView {
+
+    static content = {
+
+//        usernameField
+//        passwordField
+//        submitButton
+
+
+    }
+}
