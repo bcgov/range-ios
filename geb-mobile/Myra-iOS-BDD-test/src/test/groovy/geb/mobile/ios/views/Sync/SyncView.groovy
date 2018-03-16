e6f5aeaf6de1fa3f585b97cab0f35c7a1870802c
“ShellyXueHan” <shelly@freshworks.io>
2018-03-16 15:42:04 -0700

    added in the syncing flow

---
+++ geb-mobile/Myra-iOS-BDD-test/src/test/groovy/geb/mobile/ios/views/Sync/SyncView.groovy
@@ -0,0 +1,13 @@
+package geb.mobile.ios.views.Sync
+
+import geb.mobile.ios.IosBaseView
+
+class SyncView extends IosBaseView {
+
+    static content = {
+//        title
+//        progress???
+
+
+    }
+}
