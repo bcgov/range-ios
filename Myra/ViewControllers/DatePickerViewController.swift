2754841c6f6bd3e5502a47a5fb071d6e6add6fe5
freshamir <amir@freshworks.io>
2018-05-01 15:04:41 -0700

    Created custom picker that supports date and lists, and implemented as popover for schedule year selection

---
+++ Myra/ViewControllers/DatePickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DatePickerViewController.swift
+//  Myra
+//
+//  Created by Amir Shayegh on 2018-05-01.
+//  Copyright © 2018 Government of British Columbia. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
