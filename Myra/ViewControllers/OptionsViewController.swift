26206a66b462596ccf905e21ff284747452be8d2
freshamir <amir@freshworks.io>
2018-05-04 10:33:13 -0700

    created storyboard and viewcontroller for options popover

---
+++ Myra/ViewControllers/OptionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OptionsViewController.swift
+//  Myra
+//
+//  Created by Amir Shayegh on 2018-05-04.
+//  Copyright © 2018 Government of British Columbia. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: Class Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
