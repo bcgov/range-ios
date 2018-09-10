8bd8a720ed6516087c8a61c61d4570f285c418e1
amirshayegh <shayegh@me.com>
2018-09-10 15:56:47 -0700

    moved sync view to its own nib and removed unnecessary code from BaseViewController

---
+++ Myra/UI/Nibs/SyncView.swift
@@ -0,0 +1,21 @@
+//
+//  SyncView.swift
+//  Myra
+//
+//  Created by Amir Shayegh on 2018-09-07.
+//  Copyright © 2018 Government of British Columbia. All rights reserved.
+//
+
+import UIKit
+
+class SyncView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
