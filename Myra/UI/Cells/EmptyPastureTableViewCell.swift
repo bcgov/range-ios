4fbcaad779081a07144411072b1fb4038b762063
amirshayegh <shayegh@me.com>
2018-10-22 13:27:47 -0700

    added placeholder and required fields

---
+++ Myra/UI/Cells/EmptyPastureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmptyPastureTableViewCell.swift
+//  Myra
+//
+//  Created by Amir Shayegh on 2018-10-22.
+//  Copyright © 2018 Government of British Columbia. All rights reserved.
+//
+
+import UIKit
+
+class EmptyPastureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
