02cfa28f15352557d04c592ebe3403b239e7b771
Amir Shayegh <shayegh@me.com>
2019-03-26 12:16:53 -0700

    Styled Flows + some refactoring of constants

---
+++ Myra/Utilities/Constants/StatusConstants.swift
@@ -0,0 +1,9 @@
+//
+//  StatusConstants.swift
+//  Myra
+//
+//  Created by Amir Shayegh on 2019-03-25.
+//  Copyright © 2019 Government of British Columbia. All rights reserved.
+//
+
+import Foundation
