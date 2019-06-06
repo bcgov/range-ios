879af4b0a2056aef1998d84c930b12f8cf3593fc
Jason C. Leach <jason.leach@fullboar.ca>
2019-06-06 16:08:20 -0700

    Add SSO config

---
+++ Myra/SSOConstants.swift
@@ -0,0 +1,34 @@
+//
+//  SSOConstants.swift
+//  Myra
+//
+//  Created by Amir Shayegh on 2019-03-19.
+//  Copyright © 2019 Government of British Columbia. All rights reserved.
+//
+
+import Foundation
+
+struct SSO {
+    struct Dev {
+        static let baseUrl = URL(string: "https://sso-dev.pathfinder.gov.bc.ca")!
+    }
+    struct Test {
+        static let baseUrl = URL(string: "https://sso-test.pathfinder.gov.bc.ca")!
+    }
+    struct Prod {
+        static let baseUrl = URL(string: "https://sso.pathfinder.gov.bc.ca")!
+    }
+    
+    static var baseUrl: URL {
+        switch SettingsManager.shared.getCurrentEnvironment() {
+        case .Dev:
+            return SSO.Dev.baseUrl
+        case .Prod:
+            return SSO.Prod.baseUrl
+        }
+    }
+    static let redirectUri = "myra-ios://client"
+    static let clientId = "myrangebc"
+    static let realmName = "range"
+    static let idpHint = "idir"
+}
