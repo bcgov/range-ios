a901c1de60b6ad3f74d4587188b6529bee8e09c6
micheal-w-wells <micheal.w.wells@gmail.com>
2019-09-16 16:25:21 -0700

    add tests needed to complete guts of https://github.com/bcgov/range-ios/issues/152

--- MyraTests/MyraTests.swift
+++ MyraTests/MyraTests.swift
@@ -9,26 +9,46 @@
 import XCTest
 import Fakery
 
+
+
 class MyraTests: XCTestCase {
+   
+    static var faker = Faker()
     
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        MyraTests.faker = Faker(locale: "en-CA")
     }
     
-    func testCanCreateValidMockAgreement() {
+    func test_Can_Create_Valid_Mock_Agreement() {
         XCTAssert(false)
     }
     
-    func testCanCreateRUP() {
-        
+    func test_Can_Create_RUP_From_Mock_Agreement() {
         XCTAssert(false)
     }
     
+    func test_Can_Set_RUP_Start_Date_Past_Agreement_End_Date() {
+        XCTAssert(false)
+    }
+    
+    func test_Can_Set_RUP_End_Date_Past_Agreement_End_Date() {
+        XCTAssert(false)
+    }
+    
+    func test_Can_Create_Usage_data_extending_past_agreement_to_RUP_end_date()
+    {
+        XCTAssert(false)
+    }
+    
+    func test_Can_Identify_date_in_usage_period_as_FTA_set_or_Extended()
+    {
+        XCTAssert(false)
+    }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    }
+    
 
 }
