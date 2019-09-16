a74e930b58aa42478bc47a909c729ff8970f1c4e
micheal-w-wells <micheal.w.wells@gmail.com>
2019-09-16 14:25:21 -0700

    first add new target for unit tests

---
+++ MyraTests/MyraTests.swift
@@ -0,0 +1,37 @@
+//
+//  MyraTests.swift
+//  MyraTests
+//
+//  Created by Wells, Micheal W IIT:EX on 2019-09-16.
+//  Copyright © 2019 Government of British Columbia. All rights reserved.
+//
+
+import XCTest
+
+class MyraTests: XCTestCase {
+    
+    func testCanCreateRUP() {
+    
+    }
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
